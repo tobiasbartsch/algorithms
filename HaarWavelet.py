dc1917c6d05ebd24abb5a9c1a819166095e943d7
Tobias Bartsch <tobias-bartsch@gmx.net>
2019-07-28 08:06:48 -0400

    bug fixes

--- HaarWavelet.py
+++ HaarWavelet.py
@@ -14,5 +14,8 @@ def w1(timeseries):
 def sdevFromW1(w1):
     '''Compute the standard deviation from a w1 Haar wavelet. Sdev*sqrt(2) is 68.2% from zero for the cumulative distribution of abs(w1)'''
     sortedAbsW1 = np.sort(np.abs(w1))
-    sdev = sortedAbsW1[int(np.round(0.682*len(w1)))] / np.sqrt(2)
-    return sdev
+    if(len(sortedAbsW1)>1):
+        sdev = sortedAbsW1[int(np.round(0.682*len(w1)))] / np.sqrt(2)
+        return sdev
+    else:
+        return 0
